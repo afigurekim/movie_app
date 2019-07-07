8c6f4bb32337ead5afc72636f7f74c98baeffbc1
afigurekim <afigurekim@gmail.com>
2019-07-07 21:09:23 +0900

    Updates

---
+++ precache-manifest.111b5ca49c7ede104f353d0bcdd9f05d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3027cfdabd995109d191eb8e0bbc05bf",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "dac8ff4c36708ffd7ced",
+    "url": "/movie_app/static/css/main.95931c0f.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "dac8ff4c36708ffd7ced",
+    "url": "/movie_app/static/js/main.3f3e08b2.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
